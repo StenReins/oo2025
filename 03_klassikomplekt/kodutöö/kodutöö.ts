cd6ef19198b848a2da80281f9c2935d3015b13c0
StenReins <stenr1@tlu.ee>
2025-03-06 09:30:19 +0000

    harjutamine

--- 03_klassikomplekt/kodutöö/kodutöö.ts
+++ 03_klassikomplekt/kodutöö/kodutöö.ts
@@ -1,43 +1,61 @@
-let tasks:string[] = [];
-let result:string = "";
-let countTask:number = 0;
-
-function submitEntry(): void {
-    const eesnimi = <HTMLInputElement>document.getElementById('eesNimi');
-    const ülesanne = <HTMLInputElement>document.getElementById('ülesanne');
-    const kirjeldus = <HTMLInputElement>document.getElementById('ülesandeKirjeldus');
-    const kategooria = <HTMLSelectElement>document.getElementById('kategooria');
-
-    const task = {
-        eesnimi: eesnimi.value,
-        ülesanne: ülesanne.value,
-        kirjeldus: kirjeldus.value,
-        kategooria: kategooria.value
-    };
-
-    addTaskToList(task);
-}
-
-function addTaskToList(task): void {
-    const entries = document.getElementById('entries');
-    console.log(task);
-    countTask += 1
-    result += "<li class='progress" + countTask + "'>" + task.eesnimi + " | " + task.ülesanne + " | " + task.kirjeldus + " | " + task.kategooria + "</li>" + "<label for='progress'>Tehtud!</label><input type='checkbox' class='progress" + countTask + "' unchecked onclick='cleanTask(" + countTask + ")'><br><br>"
-    entries!.innerHTML = result;
-}
-
-/*function cleanTask(taskId:number) {
-    const taskElement = document.getElementById('task' + taskId);
-    const progressElement = document.getElementById('progress' + taskId);
-    if (taskElement) {
-        taskElement.remove(); 
-        progressElement?.remove();
+class Entry{
+    constructor(protected eesnimi:string, protected ülesanne:string, protected kirjeldus:string, protected kategooria:string, protected done:boolean ){
+        this.eesnimi = eesnimi;
+        this.ülesanne = ülesanne;
+        this.kirjeldus = kirjeldus;
+        this.kategooria = kategooria;
+        this.done = false
     }
-}*/
+}
 
-function cleanTask(id: number): void {
-    const taskElement = document.querySelector('.progress' + id)
-    if (taskElement) {
-        taskElement.remove(); 
+class Todo{
+    constructor(){
+        this.entries = JSON.parse(localStorage.getItem('entries')) || [];
+        document.querySelector('#addButton').addEventListener('click', () => {this.addEntry()});
+        this.render();
+    }
+
+    addEntry(){
+        const nameValue = <HTMLInputElement>document.querySelector('#eesNimi').value
+        const taskValue = <HTMLInputElement>document.querySelector('#ülesanne').value
+        const descriptionValue = <HTMLInputElement>document.querySelector('#ülesandeKirjeldus').value
+        const categoryValue = <HTMLSelectElement>document.querySelector('kategooria').value
+        
+        this.entries.push(new Entry(nameValue, taskValue, descriptionValue, categoryValue));
+        this.save();
+    }
+
+    render(){
+        let  tasklist = document.querySelector('#taskList');
+        if(document.querySelector('.todo-list')){
+            document.querySelector('#taskList').removeChild(document.querySelector('.todo-list'));
+        }
+
+        const ul = document.createElement('ul');
+        ul.className = "todo-list";
+
+        this.entries.forEach((entryValue, entryIndex) => {
+            const li = document.createElement('li');
+            const div = document.createElement('div');
+            const deleteButton = document.createElement('button');
+            deleteButton.innerText = "x";
+            deleteButton.addEventListener('click', () => {
+                this.entries.splice(entryIndex, 1);
+                this.save();
+            });
+            //div.innerHTML = entryValue.title + " " +  entryValue.description + " " + entryValue.date ;
+            div.innerHTML = `<div>${entryIndex + 1}.</div><div>${entryValue.eesnimi}</div><div>${entryValue.ülesanne}</div><div>${entryValue.kirjeldus}</div><div>${entryValue.kategooria}</div>`;
+
+            ul.appendChild(li);
+            li.appendChild(div);
+            li.appendChild(deleteButton);
+        });
+
+        tasklist.appendChild(ul);
+    }
+
+    save(){
+        localStorage.setItem('entries', JSON.stringify(this.entries));
+        this.render();
     }
 }
